f8e745dd36e8ed92ee7baae184abd749a63305cb
genis <gbou@inscaparrella.cat>
2025-12-01 10:57:12 +0100

    Ok
    Ok

--- src/Pages/Contacte.js
+++ src/Pages/Contacte.js
@@ -42,8 +42,77 @@ const Inici = () => {
                         <div className="card">
 
                             <div className="card-body">
-                                <h5 className="card-title">Formulari de contacte</h5>
-                                <p className="card-text">Some description here.</p>
+
+                                <div className="container">
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <div className="well well-sm">
+                                                <form className="form-horizontal" method="post">
+                                                    <fieldset>
+                                                        <legend className="text-center header">Contact us</legend>
+
+                                                        <div className="form-group">
+                                                            <span className="col-md-1 col-md-offset-2 text-center"><i
+                                                                className="fa fa-user bigicon"></i></span>
+                                                            <div className="col-md-8">
+                                                                <input id="fname" name="name" type="text"
+                                                                       placeholder="First Name"
+                                                                       className="form-control"/>
+                                                            </div>
+                                                        </div>
+                                                        <div className="form-group">
+                                                            <span className="col-md-1 col-md-offset-2 text-center"><i
+                                                                className="fa fa-user bigicon"></i></span>
+                                                            <div className="col-md-8">
+                                                                <input id="lname" name="name" type="text"
+                                                                       placeholder="Last Name"
+                                                                       className="form-control"/>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="form-group">
+                                                            <span className="col-md-1 col-md-offset-2 text-center"><i
+                                                                className="fa fa-envelope"></i></span>
+                                                            <div className="col-md-8">
+                                                                <input id="email" name="email" type="text"
+                                                                       placeholder="Email Address"
+                                                                       className="form-control"/>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="form-group">
+                                                            <span className="col-md-1 col-md-offset-2 text-center"><i
+                                                                className="fa fa-phone-square bigicon"></i></span>
+                                                            <div className="col-md-8">
+                                                                <input id="phone" name="phone" type="text"
+                                                                       placeholder="Phone" className="form-control"/>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="form-group">
+                                                            <span className="col-md-1 col-md-offset-2 text-center"><i
+                                                                className="fa fa-pencil-square-o bigicon"></i></span>
+                                                            <div className="col-md-8">
+                                                                <textarea className="form-control" id="message"
+                                                                          name="message"
+                                                                          placeholder="Enter your massage for us here. We will get back to you within 2 business days."
+                                                                          rows="7"></textarea>
+                                                            </div>
+                                                        </div>
+
+                                                        <div className="form-group">
+                                                            <div className="col-md-12 text-center">
+                                                                <button type="submit"
+                                                                        className="btn btn-primary btn-lg">Submit
+                                                                </button>
+                                                            </div>
+                                                        </div>
+                                                    </fieldset>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
